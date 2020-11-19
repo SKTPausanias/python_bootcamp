e5b9bb6f4f9ad61d42a9dba1ce30fa80e9f47021
Anias <miguellaplanamarin@gmail.com>
2020-11-19 13:00:53 +0100

    day01 ex06

--- day01/ex06/the_bank.py
+++ day01/ex06/the_bank.py
@@ -33,17 +33,41 @@ class Bank(object):
 	def add(self, account):
 		self.account.append(account)
 
+	def iscorrupted(account):
+		ret = True
+		for i in account.__dict__:
+			if i.startswith('zip') or i.startswith('addr'):
+				ret = False
+				break
+		if ret is True:
+			return ret	
+		if len(account.__dict_) % 2 == 0:
+			return True
+		for i in account.__dict__:
+			if i.startswith('b'):
+				return True
+		if 'name' not in account.__dict__:
+			return True
+		if 'id' not in account.__dict__:
+			return True
+		if 'value' not in account.__dict__:
+			return True
+		return ret
+
 	def transfer(self, origin, dest, amount):
 		orig = None
 		dst = None
-		for each in self.account:
-			if each.name == origin or each.id == origin:
-				orig = each
-			if each.name == dest or each.id == dest:
-				dst = each
+		for i in self.account:
+			if i.name == origin or i.id == origin:
+				orig = i
+			if i.name == dest or i.id == dest:
+				dst = i
 		if orig is None or dst is None:
 			print("Invalid account.")
 			return
+		if iscorrupted(orig) is True or iscorrupted(dst) is True:
+			print("Corrupted account")
+			return			
 		if orig == dst:
 			print("Invalid transaction")
 			return
@@ -55,7 +79,29 @@ class Bank(object):
 		dst.value += amount
 		print("Transaction succesful:\n" + "\tFrom {} to {}.".format(dst.name, orig.name))
 
-	#def fix_account(self, account):
+	def fix_account(self, account):
+		acc = None
+		for i in self.account:
+			if i.name == account or i.id == account
+				acc = i
+		if acc is None:
+			print("Invalid account.")
+		for i in self.__dict__:
+            if i.startswith('b'):
+                del self.__dict__[i]
+        if 'name' not in self.__dict__:
+            self.__dict__.update(name="Default")
+        if 'value' not in self.__dict__:
+            self.__dict__.update(value=0)
+        if 'id' not in self.__dict__:
+            self.__dict__.update(id=self.ID_COUNT)
+            self.ID_COUNT += 1
+        if (self.iscorrupted()):
+            return False
+        return True
+
 
 print(len(Account.__dict__))
+print(Account.__dict__)
 print(len(dir(Account)))
+print(dir(Account))
