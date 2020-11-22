7007a0b60b23f3125f6304d8c1c0820bd64569e5
Anias <miguellaplanamarin@gmail.com>
2020-11-22 21:00:04 +0100

    luL

--- day02/ex03/csvreader.py
+++ day02/ex03/csvreader.py
@@ -1,11 +1,37 @@
+#!/usr/bin/python3
 
 class CsvReader():
-	def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
-		print("h")
+	def __init__(self, filename=None, sep=',', header=True, skip_top=0, skip_bottom=0):
+		self.filename = filename
+		self.sep = sep
+		self.header = header
+		self.skip_top = skip_top
+		self.skip_bottom = skip_bottom
 	
-	def __enter__():
-	def __exit__():
+	def __enter__(self):
+		self.f = open(self.filename)
+		if (self.f is None):
+			return None
+		self.lines = self.f.readlines()
+		self.data = []
+		start = self.skip_top
+		if self.header is True:
+			self.header = self.lines[0].split(self.sep)
+			start += 1
+		end = len(self.lines) - self.skip_bottom
+		maxlen = len(self.lines[0].split(self.sep))
+		for i in range(start, end):
+			self.data.append(self.lines[i].split(self.sep))
+			for elems in self.lines[i].split(self.sep):
+				if len(elems) < maxlen:
+					return None
+		return self
+
+	def __exit__(self, exception_type, exception_value, traceback):
+		self.f.close()
 	
-	def getdata():
-	def getheader():
-	
+	def getdata(self):
+		return self.data
+
+	def getheader(self):
+		return self.header
---
+++ day02/ex04/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo hola
+tar -cvf ai42-1.0.0.tar *.py
