8c4e94cd1ecb793ca00a4009ade1720cd6f15084
Anias <miguellaplanamarin@gmail.com>
2020-11-24 13:29:25 +0100

    day03 ex03

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -8,41 +8,46 @@ class ColorFilter(object):
 		array[:, :, :3] = 1.0 - array[:, :, :3]
 		return array
 
-	@staticmethod
-	def to_blue(array):
+	def to_blue(self, array):
 		array[..., 0] = 0
 		array[..., 1] = 0
 		return array
 
-	@staticmethod
-	def to_green(array):
+	def to_green(self, array):
 		array[..., 0] = 0
 		array[..., 2] = 0
 		return array
 
-	@staticmethod
-	def to_red(array):
+	def to_red(self, array):
 		array[..., 1] = 0
 		array[..., 2] = 0
 		return array
 	
-	@staticmethod
-	def to_celluloid(array):
+	def shade(self, a, b):
+		return b * int(a / b)
+
+	def to_celluloid(self, array):
+		vfunc = np.vectorize(self.shade)
+		for i in np.linspace(0.75, 0.78, num=4):
+			array[:, :, :3] = vfunc(array[:, :, :3], (1.0 - i))
 		return array
 
 
 
 def main():
 	img = ImageProcessor()
+	cf = ColorFilter()
 	array = img.load("../assets/img.png")
 	img.display(array)
 	img.display(ColorFilter.invert(array))
 	array = img.load("../assets/img.png")
-	img.display(ColorFilter.to_blue(array))
+	img.display(cf.to_blue(array))
 	array = img.load("../assets/img.png")
-	img.display(ColorFilter.to_green(array))
+	img.display(cf.to_green(array))
 	array = img.load("../assets/img.png")
-	img.display(ColorFilter.to_red(array))
+	img.display(cf.to_red(array))
+	array = img.load("../assets/img.png")
+	img.display(cf.to_celluloid(array))
 
 
 
